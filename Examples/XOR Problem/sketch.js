a2a19d5d3eeb3f4de041e43a0b8ed05af7797f1e
Hrishikesh Shekhar <hrishikeshshekhar@gmail.com>
2018-07-05 00:09:47 +0530

    Tested and works on XOR problem. Can be used for further projects

---
+++ Examples/XOR Problem/sketch.js
@@ -0,0 +1,54 @@
+let training_data;
+let nn;
+
+function setup()
+{
+  createCanvas(400, 400);
+  nn = new Nn(2, 8, 1);
+  nn.setup();
+
+  training_data = [
+    {
+      input: [0, 0],
+      output: [0]
+    },
+    {
+      input: [0, 1],
+      output: [1]
+    },
+    {
+      input: [1, 0],
+      output: [1]
+    },
+    {
+      input: [1, 1],
+      output: [0]
+    }
+  ];
+}
+
+function draw()
+{
+  background(0);
+  for(var i = 0; i < 1000; ++i)
+  {
+    var data = random(training_data);
+    nn.train(data.input, data.output);
+  }
+
+  let resol = 20;
+  let cols = width / resol;
+  let rows = width / resol;
+  for(let x = 0; x < rows; ++x)
+  {
+    for(let y = 0; y < cols; ++y)
+    {
+      let x1 = x / rows;
+      let x2 = y / cols;
+      let inputs = [x1, x2];
+      let r = nn.predict(inputs);
+      fill(r * 255);
+      rect(x * resol, y * resol, resol, resol);
+    }
+  }
+}
